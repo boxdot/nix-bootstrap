24253ab80ed8fe244db641ed4958722baa8b878a
boxdot <d@zerovolt.org>
2015-10-16 20:15:23 +0200

    Fix typo.

--- pkgs/hi/default.nix
+++ pkgs/hi/default.nix
@@ -4,11 +4,11 @@ stdenv.mkDerivation rec {
   name = "hi-0.0.1";
 
   src = ../../hi;
-  builder = ./builder.sh;git
+  builder = ./builder.sh;
   buildInputs = [ zlib ] ++ (with llvmPackages_37; [ clang llvm ]);
 
   meta = {
-    description = "A simle Hi, World! programm.";
+    description = "A simle `Hi, World!` programm.";
     homepage = http://example.com;
     license = stdenv.lib.licenses.mit;
   };
