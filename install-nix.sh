da635dcf4cee630b13765cdda506904608db639f
boxdot <d@zerovolt.org>
2015-11-04 14:12:37 +0100

    Enable caching again.

--- install-nix.sh
+++ install-nix.sh
@@ -1,6 +1,8 @@
 set -x
 set -e
 
-curl https://nixos.org/nix/install | sh
+if [ ! -d /nix ]; then
+  curl https://nixos.org/nix/install | sh
+fi
 echo ". $HOME/.nix-profile/etc/profile.d/nix.sh" \
   | cat - $HOME/.bashrc > /tmp/out && mv /tmp/out $HOME/.bashrc
