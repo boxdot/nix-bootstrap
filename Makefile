729b65aa05fdd38e1ffff4f5b4eb1b85f5565b96
boxdot <d@zerovolt.org>
2015-11-04 15:18:03 +0100

    Try to enable full caching.

--- Makefile
+++ Makefile
@@ -1,7 +1,13 @@
 hi:
 	nix-build packages.nix -A $@
 
+
+install-deps:
+	nix-shell packages.nix -A hi --command true
+
+
 test: hi
 	result/bin/hi ||:
 
-.PHONY: hi test
+
+.PHONY: hi test install-deps
--- scripts/install-nix.sh
+++ scripts/install-nix.sh
@@ -1,14 +1,6 @@
 set -x
 set -e
 
-nix="nix-1.10-x86_64-linux"
-
-if [ ! -d $HOME/$nix ]; then
-  cd $HOME
-  wget https://nixos.org/releases/nix/nix-1.10/$nix.tar.bz2
-  tar xf $nix.tar.bz2
+if [ ! -e /nix -o ! "$(ls -A /nix)" ]; then
+  curl https://nixos.org/nix/install | sh
 fi
-
-$HOME/$nix/install
-echo ". $HOME/.nix-profile/etc/profile.d/nix.sh" \
-  | cat - $HOME/.bashrc > /tmp/out && mv /tmp/out $HOME/.bashrc
