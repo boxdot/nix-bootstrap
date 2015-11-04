aa82ecdeb99962ecfe5237494e604950fbf5c8cc
boxdot <d@zerovolt.org>
2015-11-04 14:00:27 +0100

    Disable caching.

--- install-nix.sh
+++ install-nix.sh
@@ -1,11 +1,6 @@
 set -x
 set -e
 
-if [ ! -e $HOME/nix ]; then
-  curl https://nixos.org/nix/install | sh
-  ln -sT /nix $HOME/nix
-else
-  ln -sT $HOME/nix /nix
-fi
+curl https://nixos.org/nix/install | sh
 echo ". $HOME/.nix-profile/etc/profile.d/nix.sh" \
   | cat - $HOME/.bashrc > /tmp/out && mv /tmp/out $HOME/.bashrc
