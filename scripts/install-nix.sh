d45c3758cf4b52350712d80e655bf9fd25a3ad0f
boxdot <d@zerovolt.org>
2015-11-04 15:03:08 +0100

    Add partial nix caching.

--- scripts/install-nix.sh
+++ scripts/install-nix.sh
@@ -1,6 +1,14 @@
 set -x
 set -e
 
-curl https://nixos.org/nix/install | sh
+nix="nix-1.10-x86_64-linux"
+
+if [ ! -d $HOME/$nix ]; then
+  cd $HOME
+  wget https://nixos.org/releases/nix/nix-1.10/$nix.tar.bz2
+  tar xf $nix.tar.bz2
+fi
+
+$HOME/$nix/install
 echo ". $HOME/.nix-profile/etc/profile.d/nix.sh" \
   | cat - $HOME/.bashrc > /tmp/out && mv /tmp/out $HOME/.bashrc
